7102c1905ec23a4b8b497d589c9431326444f892
Johann Costa <johansilvaa179@gmail.com>
2024-03-23 02:23:54 -0300

    Primeiro Commit

---
+++ JAVA/EX1.js
@@ -0,0 +1,15 @@
+let rn = 4;
+let rnplus = 6
+let p = 8
+let peso = 7
+
+if (peso <= rn) {
+    console.log("o tamanho ideal é o RN");
+}
+else if (peso <= rnplus) {
+    console.log("o tamanho ideal é RN+");
+}
+else if (peso <= p) {
+    console.log("o tamanho ideal é P");
+}
+else {console.log("seu filho não pode mais usar fralda");}
